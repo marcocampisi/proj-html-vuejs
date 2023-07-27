6761d78753507f8f5d27b0c579338224999ec05e
marcocampisi <marco.campisi@outlook.it>
2023-07-27 03:26:47 +0200

    Add footer container

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,7 @@
 <script>
-import { store } from '../store.js';
-export default{
+import {store} from '../store.js';
+
+export default {
     name: "FooterComponent",
     data() {
         return {
@@ -11,9 +12,81 @@ export default{
 </script>
 
 <template>
-
+    <div class="container-fluid footerContainer">
+        <div class="container-fluid d-flex justify-content-center px-5">
+            <div class="col-3">
+                <img src="../assets/img/logo-light.png" alt="iAcademy">
+                <p class="pb-5">Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life.
+                    Closer to Home</p>
+                <p>
+                    <font-awesome-icon icon="fa-solid fa-phone-volume"></font-awesome-icon>
+                    1-677-124-44227
+                </p>
+                <p>
+                    <font-awesome-icon icon="fa-regular fa-clock"></font-awesome-icon>
+                    Mon - Sat 8.00 - 18.00
+                </p>
+            </div>
+            <div class="col-3">
+                <h3>Popular Courses</h3>
+                <h5>Business English</h5>
+                <p>Preston Marshall</p>
+                <h5>Social Computing</h5>
+                <p>David Sanders</p>
+                <h5>Learn Spanish</h5>
+                <p>Jennie King</p>
+            </div>
+            <div class="col-3">
+                <h3>Support</h3>
+                <h6>User dashboard</h6>
+                <h6>Contact Us</h6>
+                <h6>FAQ</h6>
+                <h6>Course Offer</h6>
+                <h6>Events</h6>
+            </div>
+            <div class="col-3">
+                <h3>Flexible Learning</h3>
+                <img src="../assets/img/footer-img-1.png" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.footerContainer {
+    height: 600px;
+    background-color: #222222;
+    padding-top: 150px;
 
+    .col-3 {
+        > img {
+            width: 80%;
+            padding-bottom: 40px;
+        }
+
+        > p {
+            color: #ababab;
+            width: 80%;
+        }
+
+        h3 {
+            color: white;
+            font-family: "Times New Roman", 'serif';
+            font-weight: bold;
+            margin-bottom: 60px;
+        }
+
+        h5 {
+            color: #ababab;
+            font-family: "Times New Roman", 'serif';
+            font-weight: bold;
+        }
+
+        h6 {
+            color: #ababab;
+            margin-bottom: 20px;
+            cursor: pointer;
+        }
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import './assets/scss/main.scss';
 import App from './App.vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faTag } from "@fortawesome/free-solid-svg-icons";
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faBagShopping} from "@fortawesome/free-solid-svg-icons";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {faCircle} from "@fortawesome/free-solid-svg-icons";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faTag} from "@fortawesome/free-solid-svg-icons";
+import {faPhoneVolume} from "@fortawesome/free-solid-svg-icons";
+import {faClock} from "@fortawesome/free-regular-svg-icons";
 
-library.add(faSearch, faBagShopping, faBars, faChevronUp, faCircle, faUser, faTag);
+library.add(faSearch, faBagShopping, faBars, faChevronUp, faCircle, faUser, faTag, faPhoneVolume, faClock);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
